32efc83a7ee26c97a0c73e6dcec307fd1ac5dc2b
Jorge Alejandro Cardona Ortiz <64700425+Cupacowha@users.noreply.github.com>
2025-06-04 20:10:38 -0500

    Add files via upload

--- funciones.js
+++ funciones.js
@@ -1,15 +1,30 @@
-let mostrarMenu = false;
+var mostrarMenu = false;
 
 document.addEventListener("click", (e) => {
-  const menu = document.getElementById("menu");
-
+  alert(e.target);
   if (e.target.id === "icono") {
     mostrarMenu = !mostrarMenu;
 
     if (mostrarMenu) {
-      menu.style.display = "flex";
-    } else {
-      menu.style.display = "none";
+      document.getElementById("menu").style.display = "flex";
+      document.getElementById("productos").style.paddingTop = "230px";
     }
+  }
+
+  if (e.target.id === "menugrande") {
+    ocultarMenu();
+  }
+});
+
+function ocultarMenu() {
+  mostrarMenu = false;
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("productos").style.paddingTop = "0px";
+}
+
+window.addEventListener("scroll", () => {
+  if (screen.width > 768 && mostrarMenu) {
+    // Si el ancho de la pantalla es mayor a 768px y el menú está visible, ocultarlo
+    ocultarMenu();
   }
 });
